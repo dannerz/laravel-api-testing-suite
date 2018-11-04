8563315c97e0f5131af5b39dc3b7ad8d474a807a
Daniel Jones <danny@sotamedia.co.uk>
2018-11-04 11:26:58 +0000

    FiltersStringColumns

---
+++ src/Traits/FiltersStringColumns.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Dannerz\LaravelApiTestingSuite\Traits;
+
+trait FiltersStringColumns
+{
+    protected $filterStringColumns = [
+        // string_column => isNullable : boolean
+    ];
+
+    /** @test */
+    function filters_string_columns()
+    {
+        foreach ($this->filterStringColumns as $stringColumn => $isNullable) {
+
+            $models1 = factory($this->resourceModelFullClassName, 1)->create([$stringColumn => 'Aaa']);
+            $models2 = factory($this->resourceModelFullClassName, 2)->create([$stringColumn => 'Bbb']);
+            $models3 = factory($this->resourceModelFullClassName, 3)->create([$stringColumn => 'Ccc']);
+            $models4 = factory($this->resourceModelFullClassName, 4)->create([$stringColumn => $isNullable ? null : 'Ddd']);
+            $models5 = factory($this->resourceModelFullClassName, 5)->create([$stringColumn => 'Eee']);
+
+            $queryString = '?filter['.$stringColumn.']=Aaa,'.($isNullable ? 'null' : 'Ddd');
+
+            $response = $this->callRoute($queryString);
+
+            $response->assertStatus(200)->assertJsonCount(5, 'data');
+
+            $this->assertEquals($models1->merge($models4)->fresh()->toArray(), $response->json('data'));
+
+            $this->emptyResourceModelTable();
+        }
+    }
+}
