974b02efc4f3ad2d687500e3eb75fdbd8388294a
Daniel Jones <danny@sotamedia.co.uk>
2018-11-04 18:08:53 +0000

    SortsStringColumns

---
+++ src/Traits/SortsStringColumns.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Dannerz\LaravelApiTestingSuite\Traits;
+
+use Illuminate\Database\Eloquent\Collection;
+
+trait SortsStringColumns
+{
+    protected $sortStringColumns = [];
+
+    /** @test */
+    function sorts_string_columns()
+    {
+        foreach ($this->sortStringColumns as $stringColumn) {
+
+            $model6 = factory($this->resourceModelFullClassName)->create([$stringColumn => 'Aaf']);
+            $model2 = factory($this->resourceModelFullClassName)->create([$stringColumn => 'Aab']);
+            $model1 = factory($this->resourceModelFullClassName)->create([$stringColumn => 'Aaa']);
+            $model8 = factory($this->resourceModelFullClassName)->create([$stringColumn => 'Aah']);
+            $model3 = factory($this->resourceModelFullClassName)->create([$stringColumn => 'Aac']);
+            $model7 = factory($this->resourceModelFullClassName)->create([$stringColumn => 'Aag']);
+            $model4 = factory($this->resourceModelFullClassName)->create([$stringColumn => 'Aad']);
+            $model5 = factory($this->resourceModelFullClassName)->create([$stringColumn => 'Aae']);
+
+            // Asc.
+            $queryString = '?sort='.$stringColumn;
+            $response = $this->callRoute($queryString);
+            $response->assertStatus(200);
+            $this->assertEquals(
+                (new Collection([$model1, $model2, $model3, $model4, $model5, $model6, $model7, $model8]))->fresh()->toArray(),
+                $response->json('data')
+            );
+
+            // Desc.
+            $queryString = '?sort=-'.$stringColumn;
+            $response = $this->callRoute($queryString);
+            $response->assertStatus(200);
+            $this->assertEquals(
+                (new Collection([$model8, $model7, $model6, $model5, $model4, $model3, $model2, $model1]))->fresh()->toArray(),
+                $response->json('data')
+            );
+
+            $this->emptyResourceModelTable();
+        }
+    }
+}
